6139b8bb3e08977271377b3d3f7b6067f81b256e
Assad Isah <nottherealalanturing@gmail.com>
2022-05-17 22:26:04 +0100

    feat: add empty modal

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import './index.css';
-import displayChar from './modules/display-items.js';
+import { displayChar, showComment } from './modules/display-items.js';
+import { modal, btn } from './modules/DOM-elements.js';
 
 const pokemons = ['pikachu', 'bulbasaur', 'venusaur', 'pidgeot', 'butterfree', 'spearow'];
+
 pokemons.forEach((name) => {
   displayChar(name);
+});
+
+btn.addEventListener('click', () => {
+  showComment();
+  const span = document.createElement('span');
+  span.innerHTML = '&times;';
+  span.classList.add('close');
+  span.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+  modal.children[0].append(span);
+  modal.style.display = 'block';
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
 });
--- src/modules/display-items.js
+++ src/modules/display-items.js
@@ -1,4 +1,4 @@
-import { cardContainer, logoContainer } from './DOM-elements.js';
+import { cardContainer, logoContainer, modal } from './DOM-elements.js';
 import logo from '../assets/logo.jpg';
 
 logoContainer.src = logo;
@@ -7,6 +7,7 @@ function displayCards(data) {
   const imgUrl = data.sprites.other['official-artwork'].front_default;
   const { name } = data;
   const card = document.createElement('div');
+  card.dataset.name = name;
   card.classList.add('card');
   card.innerHTML = `<img id="card-img" src=${imgUrl} alt="">
           <div class="name-heart">
@@ -14,12 +15,12 @@ function displayCards(data) {
           <i class="fa-regular fa-heart"></i>
           </div>
           <div class="likes">5 likes</div>
-          <div class="comments">Comments</div>
-          <div class="reservations">Reservations</div>`;
+          <button type='button' class="comments">Comments</button>
+          <button type='button' class="reservations">Reservations</button>`;
   cardContainer.appendChild(card);
 }
 
-export default function displayChar(name) {
+export function displayChar(name) {
   const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
   fetch(url)
     .then((response) => response.json())
@@ -28,3 +29,12 @@ export default function displayChar(name) {
     })
     .catch((err) => `Pokemon not found, ${err}`);
 }
+
+export const showComment = () => {
+  const comment = `
+  <div class="modal-content">
+    <p>Some text in the Modal..</p>
+  </div>
+`;
+  modal.innerHTML = comment;
+};
