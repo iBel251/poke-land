e074eb853a5530edf61914311748b62dfd9e5caf
iBel251 <ibbimran7@gmail.com>
2022-05-18 12:13:00 +0300

    Display likes from Api response

---
+++ src/modules/display-likes.js
@@ -0,0 +1,27 @@
+const apiUrl = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ZoxDLHcPIRPn5ap2fi3h/likes`;
+const gameId = "ZoxDLHcPIRPn5ap2fi3h";
+
+export async function getLike() {
+    const replay = await fetch(apiUrl);
+    const data = await replay.json()
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     displayLike(data);
+    //   })
+    //   .catch((err) => `Pokemon not found, ${err}`);
+    return data;
+  }
+
+  export async function createLike(name) {
+    await fetch(apiUrl, {
+      method: 'POST',
+      body: JSON.stringify({ "item_id": name }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+async function displayLike(data) {
+    const
+}
